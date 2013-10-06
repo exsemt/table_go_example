2f82a33688ff1122dd5c485794b24fd51b2f1d54
exsemt <exsemt@gmail.com>
2013-10-06 18:54:16 +0200

    app config: test and locales

--- config/application.rb
+++ config/application.rb
@@ -14,10 +14,20 @@ module TableGoExample
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Berlin'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = [:de, :en]
     config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
+    config.generators do |g|
+      g.test_framework      :rspec
+      g.fixture_replacement :factory_girl
+      g.template_engine     :haml
+      g.view_specs          false
+      g.stylesheets         false
+    end
   end
 end
