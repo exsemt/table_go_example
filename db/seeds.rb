d68f7377d80942d938dd9065bd17dd3f5def8673
Vladimir Glusic <exsemt@gmail.com>
2018-11-07 21:49:44 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,38 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# [Author, Book, Publisher].each(&:delete_all)
+
+puts 'Start...'
+5.times do
+  Publisher.create(
+    name: Faker::Company.name,
+    place: Faker::Address.city
+  )
+end
+puts 'Publisher created'
+publisher_ids = Publisher.ids
+
+20.times do
+  author = Author.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    birthday: rand(Date.new(1950)..Date.new(1990)),
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.phone_number
+  )
+  10.times do
+    Book.create(
+      authors: [author],
+      title: Faker::Company.name,
+      isbn: Faker::Number.number(9),
+      pages: Faker::Number.number(2),
+      edition: Faker::Number.number(3),
+      price: [2.99, 4.99, 8.95, 9.95, 19.95, 29.95, 39.95, 49.95, 59.95],
+      release_date: rand(Date.new(1980)..Time.zone.today),
+      publisher_id: publisher_ids.sample
+    )
+  end
+end
+puts 'Finish'
